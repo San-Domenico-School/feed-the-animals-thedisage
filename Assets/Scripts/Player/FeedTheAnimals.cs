2ec827250bf88dfe7757124112832dc12ab379fa
thedisage <thedisage@gmail.com>
2025-08-27 10:59:33 -0700

    Made Simple code

---
+++ Assets/Scripts/Player/FeedTheAnimals.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+/*
+ * Class: FeedTheAnimals
+ * Purpose: Handle feeding of animals.
+ * Component of: Player
+ * Fields
+ *  -Food (serialize field) → sets a GameObject as food
+ *  -Force (float) → Controls force with which food is thrown
+ *  Behaviors
+ *  - Start() → Initializes variables.  
+ *  - Update() → Executes the PlayerMovement methods per frame.
+ *  - OnFeedAnimalEnter() → ???
+ *  - FeedAnimal() → Feeds animal???
+ *  Author: Sufian St. Denny+ Bruce Gustin 
+ *  Version: August 27, 2025
+ */
+public class FeedTheAnimals : MonoBehaviour
+{
+    [SerializeField] private Food;
+    private float Force;
+    void OnFeedAnimalEnter()
+    {
+
+    }
+    void FeedAnimal()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
