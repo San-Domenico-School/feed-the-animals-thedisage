321acb27d8e62740f350775e8a16f66340c75e75
thedisage <thedisage@gmail.com>
2025-09-23 11:00:49 -0700

    Half of feed the animals (up to lesson 6)

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,25 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-/******************************************************************************  
- * Class: PlayerMovementController  
- * Purpose: Controls player movement based on input.  
- * Component Of: Player GameObject  
- * Fields: 
- *   - playerSpeed (float) → Controls movement speed.  
- *   - widthOfField (float) → Prevents movement beyond boundaries.  
- *   - moveDirection (float) → Stores input direction.    
- * Behaviors:
- *   - Start() → Initializes variables.  
- *   - Update() → Executes the PlayerMovement methods per frame.
- *   - OnMovementInput() → Handles player input events.
- *   - DeterminePlayerDirection() → Assigns player's move direction -1, 1, or 0 
- *                                  to determine the direction of motion: left, 
- *                                  right, or stationary.
- *   - PlayerMovement() → Processes movement logic.
- * Access: To enforce encapsulation only OnMovementInput() is visible
- * Author: Sufian St. Denny
- * Version: July 1, 2025 v. 1.0
- *******************************************************************************/
+
 
 public class PlayerMovementController : MonoBehaviour
 {
@@ -55,8 +36,15 @@ public class PlayerMovementController : MonoBehaviour
     //Process movement logic
     private void PlayerMovement()
     {
-        transform.Translate(Vector3.right * playerSpeed * moveDirection * Time.deltaTime);
+        if (transform.position.x < centerToEdge && moveDirection > 0 || transform.position.x > -centerToEdge && moveDirection < 0)
+
+
+        {
+            transform.Translate(Vector3.right * playerSpeed * moveDirection * Time.deltaTime);
+        }
 
     }
 
 }
+
+
