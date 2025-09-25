6d96dec8657783c506814c2d8f54d8fb56f2f58a
thedisage <thedisage@gmail.com>
2025-09-25 11:00:17 -0700

    Added Max force, doesnt work

--- Assets/Scripts/Player/FeedTheAnimals.cs
+++ Assets/Scripts/Player/FeedTheAnimals.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+
 /*
  * Class: FeedTheAnimals
  * Purpose: Handle feeding of animals.
@@ -7,35 +7,39 @@ using UnityEngine;
  *  -Food (serialize field) → sets a GameObject as food
  *  -Force (float) → Controls force with which food is thrown
  *  Behaviors
- *  - Start() → Initializes variables.  
- *  - Update() → Executes the PlayerMovement methods per frame.
- *  - OnFeedAnimalEnter() → ???
- *  - FeedAnimal() → Feeds animal???
+ *  - OnFeedAnimalEnter() → Instantiates food
+ *  - FeedAnimal() → Runs FeedAnimal on press
  *  Author: Sufian St. Denny
  *  Version: August 27, 2025
  */
+using UnityEngine;
+using UnityEngine.InputSystem;
 public class FeedTheAnimals : MonoBehaviour
 {
     [SerializeField] private GameObject food;
-    private float maxForce;
-    private AudioSource audiaSource;
-    public void OnFeedInput()
-    {
+    [SerializeField] private float maxForce;
+    [SerializeField] private AudioSource audioSource;
 
-    }
-    private void FeedAnimal()
-    {
+    //public fields
 
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void FeedAnimal(string name)
     {
-        
+        Vector3 position = transform.position + new Vector3(0, 2, 0); //Sets position 2 meters above center of Player
+        GameObject foodInstance = Instantiate(food, position, Quaternion.identity);  //Adds food prefab to world
+        Rigidbody foodRB = foodInstance.GetComponent<Rigidbody>();    //Set rigidbody of food
+        foodRB.AddForce(Vector3.forward * maxForce, ForceMode.Impulse);  //Adds a forward impulse force to the                      
+        audioSource.Play();                                                                 //food's rigidbody
+    }
+    //dynamic method that gets binding from player input map
+    public void OnFeedInput(InputAction.CallbackContext ctx)
+    {
+        //Only feeds animals on start press.  Ignores ctx.proceed and ctx.cancel.
+        if (ctx.started)
+        {
+            //Send name of button pressed to FeedAnimal
+            FeedAnimal(ctx.control.name);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
