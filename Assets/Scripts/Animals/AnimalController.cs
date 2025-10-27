290a49c41ff76a1149c96ee86b77f3e0f2f89bc3
thedisage <thedisage@gmail.com>
2025-10-27 11:02:08 -0700

    Step 6 (doesnt work)

--- Assets/Scripts/Animals/AnimalController.cs
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -1,3 +1,17 @@
+
+/*
+ * Class: AnimalController
+ * Purpose: Controlling Animals
+ * Component of: Animals
+ * Fie
+ *  Behaviors
+ *  -MoveForward → Moves Animal
+ *  -DeleteOutOfScene → Deletes Animal out of scene
+ *  - OnFeedAnimalEnter() → Instantiates food
+ *  - FeedAnimal() → Runs FeedAnimal on press
+ *  Author: Sufian St. Denny
+ *  Version: October 27, 2025
+ */
 using UnityEngine;
 public class AnimalControler : MonoBehaviour
 {
@@ -10,13 +24,14 @@ public class AnimalControler : MonoBehaviour
      // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        lowerBound = -15f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveForward();
+        DeleteOutOfScene();
     }
     
     private void MoveForward()
@@ -26,16 +41,38 @@ public class AnimalControler : MonoBehaviour
 
     private void DeleteOutOfScene()
     {
+     
+      if (transform.position.z < lowerBound)
 
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private bool IsFoodItEats()
+    private bool IsFoodItEats(string foodTriggered)
     {
-        return false;
+        string foodItEatsName = foodItEats.name;
+        //remove Clones
+        int cloneIndex = foodTriggered.IndexOf("(Clone)");
+        if (cloneIndex != -1)
+        {
+            foodTriggered = foodTriggered.Substring(0, cloneIndex).Trim();
+        }
+        return foodTriggered.Equals(foodItEatsName);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.CompareTag("Food")&& !notHungry)
+        {
+            IsFoodItEats();
+          if (IsFoodItEats = true)
+            {
+                Debug.Log($"Is FoodItEats");
+            }
+              
+
+        }
         
     }
 
